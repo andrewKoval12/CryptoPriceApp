627e26278ebc3f8a59510cf2226fe9be78ccc727
andrewKoval12 <andrey.koval1212@gmail.com>
2022-01-16 12:30:21 +0200

    - Refactor CoinInfoAdapter.kt

--- app/src/main/java/ua/com/koval/andrey/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/ua/com/koval/andrey/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -2,17 +2,17 @@ package ua.com.koval.andrey.cryptoapp.presentation.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ua.com.koval.andrey.cryptoapp.R
 import ua.com.koval.andrey.cryptoapp.databinding.ItemCoinInfoBinding
 import ua.com.koval.andrey.cryptoapp.domain.CoinInfo
+import java.lang.String.format
 
 
 class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+    RecyclerView.Adapter<CoinInfoViewHolder>() {
 
     var onCoinClickListener: OnCoinClickListener? = null
     var coinInfoList = arrayListOf<CoinInfo>()
@@ -24,41 +24,38 @@ class CoinInfoAdapter(private val context: Context) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
         val binding =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_coin_info, parent, false)
+            ItemCoinInfoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         return CoinInfoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        holder.bind(coinInfoList[position])
-    }
-
-    override fun getItemCount() = coinInfoList.size
-
-
-    inner class CoinInfoViewHolder(item: View) :
-        RecyclerView.ViewHolder(item) {
-        private val binding = ItemCoinInfoBinding.bind(item)
-
-        fun bind(coinPriceInfo: CoinInfo) {
-            with(coinPriceInfo) {
-                with(binding) {
-                    val symbolsTemplate = context.resources.getString(R.string.symbols_template)
-                    val lastUpdateTemplate =
-                        context.resources.getString(R.string.last_update_template)
-                    tvSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
-                    tvPrice.text = price
-                    tvLastUpdate.text =
-                        String.format(lastUpdateTemplate, lastUpdate)
-                    Glide.with(context).load(imageUrl).into(ivLogoCoin)
-                    itemView.setOnClickListener {
-                        onCoinClickListener?.onCoinClick(coinPriceInfo)
-                    }
+        val coin = coinInfoList[position]
+        with(holder.binding) {
+            with(coin) {
+                val symbolsTemplate =
+                    context.resources.getString(R.string.symbols_template)
+                val lastUpdateTemplate =
+                    context.resources.getString(R.string.last_update_template)
+                tvSymbols.text = format(symbolsTemplate, fromSymbol, toSymbol)
+                tvPrice.text = price
+                tvLastUpdate.text = format(lastUpdateTemplate, lastUpdate)
+                Glide.with(context).load(imageUrl).into(ivLogoCoin)
+                root.setOnClickListener {
+                    onCoinClickListener?.onCoinClick(this)
                 }
             }
         }
     }
 
+    override fun getItemCount() = coinInfoList.size
+
+
     interface OnCoinClickListener {
         fun onCoinClick(coinPriceInfo: CoinInfo)
     }
+
 }
---
+++ app/src/main/java/ua/com/koval/andrey/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,7 @@
+package ua.com.koval.andrey.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import ua.com.koval.andrey.cryptoapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(val binding: ItemCoinInfoBinding) :
+    RecyclerView.ViewHolder(binding.root)
