ca8e89a8619952d04166387e188b100398f62a71
andrewKoval12 <andrey.koval1212@gmail.com>
2022-01-16 15:29:11 +0200

    - Add CoinDetailFragment.kt

--- app/src/main/java/ua/com/koval/andrey/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/ua/com/koval/andrey/cryptoapp/presentation/CoinDetailActivity.kt
@@ -4,48 +4,29 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
+import ua.com.koval.andrey.cryptoapp.R
 import ua.com.koval.andrey.cryptoapp.databinding.ActivityCoinDetailBinding
 
 class CoinDetailActivity : AppCompatActivity() {
-
-
-    private lateinit var viewModel: CoinViewModel
 
     private val binding by lazy { ActivityCoinDetailBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setData()
-    }
-
-    private fun setData() {
         if (!intent.hasExtra(EXTRA_FROM_SYMBOL)) {
             finish()
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(
-            this,
-            ViewModelProvider.AndroidViewModelFactory(application)
-        )[CoinViewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this) {
-            with(binding) {
-                with(it) {
-                    tvPrice.text = price
-                    tvMinPrice.text = lowDay
-                    tvMaxPrice.text = highDay
-                    tvLastMarket.text = lastMarket
-                    tvLastUpdate.text = lastUpdate
-                    tvFromSymbol.text = fromSymbol
-                    tvToSymbol.text = toSymbol
-                    Glide.with(this@CoinDetailActivity).load(imageUrl).into(ivLogoCoin)
-                }
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
+
 
     companion object {
         private const val EXTRA_FROM_SYMBOL = "fSym"
